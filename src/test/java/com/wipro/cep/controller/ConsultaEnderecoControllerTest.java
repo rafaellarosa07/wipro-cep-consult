b6ee02abe629f1ccf6c8be1a7b560fea10705c15
rafaellarosa <rafaellarosa07@gmail.cm>
2023-03-02 12:18:36 -0400

    unit tests

--- src/test/java/com/wipro/cep/controller/ConsultaEnderecoControllerTest.java
+++ src/test/java/com/wipro/cep/controller/ConsultaEnderecoControllerTest.java
@@ -1,17 +1,57 @@
 package com.wipro.cep.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wipro.cep.configuration.ApplicationConfig;
+import com.wipro.cep.dto.RequestCepDto;
+import com.wipro.cep.dto.ResponseEnderecoDto;
+import com.wipro.cep.service.ConsultaEnderecoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@Import(ApplicationConfig.class)
 class ConsultaEnderecoControllerTest {
+
+    private static final String PATH = "/consulta-endereco";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private ConsultaEnderecoService consultaEnderecoService;
+
+    private RequestCepDto requestCepDto;
+
+    private ResponseEnderecoDto responseEnderecoDto;
 
     @BeforeEach
     void setUp() {
+        requestCepDto = new RequestCepDto("70640028");
+        responseEnderecoDto = new ResponseEnderecoDto();
     }
 
     @Test
-    void consultar() {
+    void consultar() throws Exception {
+        when(consultaEnderecoService.consultar(requestCepDto)).thenReturn(responseEnderecoDto);
+        mockMvc.perform(post(PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(responseEnderecoDto)))
+                .andExpect(status().isCreated());
     }
 }
--- src/test/java/com/wipro/cep/service/impl/CalculoFreteServiceImplTest.java
+++ src/test/java/com/wipro/cep/service/impl/CalculoFreteServiceImplTest.java
@@ -1,17 +1,34 @@
 package com.wipro.cep.service.impl;
 
+import com.wipro.cep.exception.messages.Messages;
+import com.wipro.cep.service.CalculoFreteService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class CalculoFreteServiceImplTest {
 
-    @BeforeEach
-    void setUp() {
+    @InjectMocks
+    private CalculoFreteServiceImpl calculoFreteServiceImpl;
+
+    @Test
+    void getValorFreteSuccessful() {
+        BigDecimal valorFrete = calculoFreteServiceImpl.getValorFrete("Centro-Oeste");
+        assertNotNull(valorFrete);
     }
 
     @Test
-    void getValorFrete() {
+    void getValorFreteError() {
+        assertThrows(IllegalArgumentException.class,
+                () -> calculoFreteServiceImpl.getValorFrete("xxx"));
+
     }
 }
